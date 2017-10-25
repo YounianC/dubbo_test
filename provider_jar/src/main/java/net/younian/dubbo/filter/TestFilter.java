a9a3e964d542724d1017b9abda51a20bf6473871
Younianlu <younianlu@lagou.com>
2017-10-25 16:13:42 +0800

    add timeout and another way to add filter

--- provider_jar/src/main/java/net/younian/dubbo/filter/TestFilter.java
+++ provider_jar/src/main/java/net/younian/dubbo/filter/TestFilter.java
@@ -1,11 +1,14 @@
 package net.younian.dubbo.filter;
 
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.*;
 
+@Activate(group = {Constants.PROVIDER}, order = 1)
 public class TestFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        System.out.println("TestFilter filter is called..");
+        //System.out.println("TestFilter filter is called..");
         return invoker.invoke(invocation);
     }
 }
